b41bca3e0ed3c212e158b2b8f97d27df0e6462b0
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-05-16 23:34:14 +0300

    gh-pages1 add index.html

--- src/services/changeCssVariables.js
+++
@@ -1,9 +0,0 @@
-export const changeCssVAriables = (theme) => {
-	const root = document.querySelector(":root");
-
-    const arrCssProp = ['header', 'bdimage']
-    arrCssProp.forEach((el) => {
-        root.style.setProperty(`--theme-default-${el}`, `var(--theme-${theme}-${el})`)
-    })
-
-};
--- src/store/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-export const logger = (store) => (next) => (action) => {
-    // console.group(action.type);
-    // console.info('dispatching', action);
-    // console.log('prev state', store.getState()) // Выводим предыдущее состояние в консоль
-    const result = next(action);
-    // console.log('next state', store.getState()); // Выводим новое состояние в консоль
-    // console.groupEnd();
-    return result;
-};
